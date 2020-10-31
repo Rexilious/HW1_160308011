7da43368db8dd82ed6b1b89628d87f06dd46d42c
murat <mbaran98@hotmail.com>
2020-10-31 23:01:27 +0300

    Initial Commit

---
+++ app/src/main/java/ise308/baran/murat/cardv/MainActivity.kt
@@ -0,0 +1,11 @@
+package ise308.baran.murat.cardv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+    }
+}
